ce12187de6e8c9c5dcc9bc765ebf8c1f3c338239
shrimpmode <jsilva@uni.pe>
2024-04-23 12:45:12 -0500

    refresh token

--- src/client/apiClient.ts
+++ src/client/apiClient.ts
@@ -1,9 +1,25 @@
-import wretch from 'wretch';
-import { getToken } from './authActions'
+import wretch, { Wretch, WretchError } from 'wretch';
+import { getToken, removeTokens, storeToken } from './authActions'
 
 const api = () => {
 	return wretch('http://localhost:8000/api')
 		.auth(`Bearer ${getToken("access")}`)
+		.catcher(401, async (error: WretchError, request: Wretch) => {
+			try {
+				const { access } = await wretch('http://localhost:8000/api/token/refresh/').post({
+					refresh: getToken("refresh")
+				}).json().catch(() => {
+					removeTokens()
+					window.location.replace('/login')
+					console.log('fail refresh')
+				})
+				console.log(access)
+				storeToken(access, "access")
+
+			} catch (error) {
+
+			}
+		})
 }
 
 
